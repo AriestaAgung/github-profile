f593d90c0ddae7d33e93fae189acbc2bcdcad9fd
Ariesta <ariesta.work@gmail.com>
2024-04-23 09:32:11 +0700

    Adding repo card component and network manager

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
 import BottomBar from "@/components/bottom-bar";
 import Navbar from "@/components/navbar";
+import RepoCard from "@/components/repo-card";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
       <Navbar></Navbar>
+      <RepoCard
+        username="User"
+        repoName="Repository"
+        stars="4k"
+        desc="This is a user repository"
+      />
       <BottomBar />
     </main>
   );
