23b2f859c2f75fafc463c5037670e0dbb16152fe
Ariesta <ariesta.work@gmail.com>
2024-04-27 11:30:07 +0700

    Wrap link inside repo card component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,62 +7,24 @@ export default function Home() {
   return (
     <main className="mt-24 mb-24">
       <Navbar></Navbar>
-      <div>
-        <RepoCard
-          username="User"
-          repoName="Repository"
-          stars="4k"
-          desc="This is a user repository"
-        />
-        <RepoCard
-          username="User"
-          repoName="Repository"
-          stars="4k"
-          desc="This is a user repository"
-        />
-        <RepoCard
-          username="User"
-          repoName="Repository"
-          stars="4k"
-          desc="This is a user repository"
-        />
-        <RepoCard
-          username="User"
-          repoName="Repository"
-          stars="4k"
-          desc="This is a user repository"
-        />
-        <RepoCard
-          username="User"
-          repoName="Repository"
-          stars="4k"
-          desc="This is a user repository"
-        />
-        <RepoCard
-          username="User"
-          repoName="Repository"
-          stars="4k"
-          desc="This is a user repository"
-        />
-        <RepoCard
-          username="User"
-          repoName="Repository"
-          stars="4k"
-          desc="This is a user repository"
-        />
-        <RepoCard
-          username="User"
-          repoName="Repository"
-          stars="4k"
-          desc="This is a user repository"
-        />
-        <RepoCard
-          username="User"
-          repoName="Repository"
-          stars="4k"
-          desc="This is a user repository"
-        />
-      </div>
+      {repoData.map(
+        (item: {
+          html_url: string;
+          description: string;
+          owner: any;
+          name: string;
+        }) => {
+          return (
+            <RepoCard
+              username={item.owner.login}
+              repoName={item.name}
+              stars="4k"
+              desc={item.description}
+              href={item.html_url}
+            />
+          );
+        }
+      )}
       <BottomBar />
     </main>
   );
--- src/utils/network-manager.ts
+++ src/utils/network-manager.ts
@@ -1,8 +1,32 @@
 export async function NetworkManager(request: any) {
     const res = await fetch("", request)
 
-    if(!res.ok) {
-        throw new Error("n/a")
+export const BASE_URL = "https://api.github.com/"
+export const URL_REPO_LIST = "repositories"
+
+class NetworkManager {
+    
+    constructor(){}
+
+    async getWithRequest(endpoint: string, request: any) {
+        if (request == null){
+            const res = await fetch(BASE_URL + endpoint, {
+                headers: {
+                    'Authorization':'Bearer ' + process.env.API_KEY,
+                },
+            })
+            if(!res.ok) {
+                throw new Error("n/a")
+            }
+            return res.json()    
+        } 
     }
-    return res.json()
+    
+    async get(endpoint: string) {
+        const res =  await fetch(BASE_URL + endpoint)
+        if (!res.ok) {
+            throw new Error('Failed to fetch data')
+        }
+        return res.json()
+    }
 }
