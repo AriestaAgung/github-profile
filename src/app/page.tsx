d78321d2ce268146a4863a980e1faa4c93edcf85
Ariesta <ariesta.work@gmail.com>
2024-04-27 18:21:14 +0700

    Adding animation for loading state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,34 +2,15 @@ import BottomBar from "@/components/bottom-bar";
 import Navbar from "@/components/navbar";
 import RepoCard from "@/components/repo-card";
 import networkMgr, { URL_REPO_LIST } from "@/utils/network-manager";
-import { Url } from "next/dist/shared/lib/router/router";
-import Image from "next/image";
-import Link from "next/link";
+import GithubRepos from "@/utils/repos";
+import { QueryClientProvider, useQuery } from "@tanstack/react-query";
 
 export default async function Home() {
   const repoData = await networkMgr.get(URL_REPO_LIST);
-
   return (
     <main className="mt-24 mb-24">
       <Navbar></Navbar>
-      {repoData.map(
-        (item: {
-          html_url: string;
-          description: string;
-          owner: any;
-          name: string;
-        }) => {
-          return (
-            <RepoCard
-              username={item.owner.login}
-              repoName={item.name}
-              stars="4k"
-              desc={item.description}
-              href={item.html_url}
-            />
-          );
-        }
-      )}
+      <RepoCard data={repoData} />
       <BottomBar />
     </main>
   );
