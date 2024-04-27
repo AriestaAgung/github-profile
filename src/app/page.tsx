d91777945869d9db09fc915d6a992abe2f517239
Ariesta <ariesta.work@gmail.com>
2024-04-27 11:15:53 +0700

    Add capability to fetch github repos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,68 +1,36 @@
 import BottomBar from "@/components/bottom-bar";
 import Navbar from "@/components/navbar";
 import RepoCard from "@/components/repo-card";
+import networkMgr, { URL_REPO_LIST } from "@/utils/network-manager";
+import { Url } from "next/dist/shared/lib/router/router";
 import Image from "next/image";
+import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  const repoData = await networkMgr.get(URL_REPO_LIST);
+
   return (
     <main className="mt-24 mb-24">
       <Navbar></Navbar>
-      <div>
-        <RepoCard
-          username="User"
-          repoName="Repository"
-          stars="4k"
-          desc="This is a user repository"
-        />
-        <RepoCard
-          username="User"
-          repoName="Repository"
-          stars="4k"
-          desc="This is a user repository"
-        />
-        <RepoCard
-          username="User"
-          repoName="Repository"
-          stars="4k"
-          desc="This is a user repository"
-        />
-        <RepoCard
-          username="User"
-          repoName="Repository"
-          stars="4k"
-          desc="This is a user repository"
-        />
-        <RepoCard
-          username="User"
-          repoName="Repository"
-          stars="4k"
-          desc="This is a user repository"
-        />
-        <RepoCard
-          username="User"
-          repoName="Repository"
-          stars="4k"
-          desc="This is a user repository"
-        />
-        <RepoCard
-          username="User"
-          repoName="Repository"
-          stars="4k"
-          desc="This is a user repository"
-        />
-        <RepoCard
-          username="User"
-          repoName="Repository"
-          stars="4k"
-          desc="This is a user repository"
-        />
-        <RepoCard
-          username="User"
-          repoName="Repository"
-          stars="4k"
-          desc="This is a user repository"
-        />
-      </div>
+        {repoData.map(
+          (item: {
+            html_url: Url;
+            description: string;
+            owner: any;
+            name: string;
+          }) => {
+            return (
+              <Link href={item.html_url}>
+                <RepoCard
+                  username={item.owner.login}
+                  repoName={item.name}
+                  stars="4k"
+                  desc={item.description}
+                />
+              </Link>
+            );
+          }
+        )}
       <BottomBar />
     </main>
   );
